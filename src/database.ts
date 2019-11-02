2898fb8bfd5a8bc2e88c3f6fbd135217a9006004
Josh <joshgbuckner@gmail.com>
2019-11-02 03:17:26 -0500

    project setup

---
+++ src/database.ts
@@ -0,0 +1,52 @@
+import { verbose } from "sqlite3";
+
+const sqlite3 = verbose();
+
+const DBSOURCE = "db.sqlite";
+
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // tslint:disable-next-line: no-console
+    console.log(err);
+    return;
+  }
+  // tslint:disable-next-line: no-console
+  console.log("Connected to " + DBSOURCE + " database.");
+  db.exec("PRAGMA foreign_keys = ON;", (error) => {
+    if (error) {
+      // tslint:disable-next-line: no-console
+      console.error("Pragma statement didn't work.");
+    } else {
+      // tslint:disable-next-line: no-console
+      console.log("Foreign Key Enforcement is on.");
+    }
+  });
+});
+
+const dbSchema = `CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name text,
+  email text UNIQUE,
+  password text,
+  access_token text UNIQUE,
+  CONSTRAINT email_unique UNIQUE (email)
+);
+CREATE TABLE IF NOT EXISTS events (
+  event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  message text,
+  file text,
+  line text,
+  column text,
+  error text,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);`;
+
+db.exec(dbSchema, (err) => {
+  if (err) {
+    // tslint:disable-next-line: no-console
+    console.log(err);
+  }
+});
+
+export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import md5 from "md5";
+import db from "./database";
 
 // init express
 const app = express();
@@ -14,10 +16,106 @@ app.get("/", (req, res) => {
   return res.send("Hi!");
 });
 
-app.post("/api/:id/error", (req, res) => {
-  if (req.params.id === "an34k2n3sdkfj3kl2") {
-    console.log(req.body);
+app.get("/api/users", (req, res, next) => {
+  const sql = "select * from users";
+  const params: any[] = [];
+  db.all(sql, params, (err: { message: any }, rows: any) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      data: rows,
+      message: "success",
+    });
+  });
+});
+
+app.post("/api/user/", (req, res, next) => {
+  const errors = [];
+  if (!req.body.access_token) {
+    errors.push("No access_token specified");
   }
+  if (!req.body.password) {
+    errors.push("No password specified");
+  }
+  if (!req.body.email) {
+    errors.push("No email specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  const data = {
+    access_token: req.body.access_token,
+    email: req.body.email,
+    name: req.body.name,
+    password: md5(req.body.password),
+  };
+  const sql = "INSERT INTO users (name, email, password, access_token) VALUES (?,?,?,?)";
+  const params = [data.name, data.email, data.password, data.access_token];
+  db.run(sql, params, function(err: { message: any; }, result: any) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      data,
+      id: this.lastID,
+      message: "success",
+    });
+  });
+});
+
+app.post("/api/event/:id/", (req, res) => {
+  const errors = [];
+  if (!req.body.message) {
+    errors.push("No message specified");
+  }
+  if (!req.body.file) {
+    errors.push("No file specified");
+  }
+  if (!req.body.line) {
+    errors.push("No line specified");
+  }
+  if (!req.body.column) {
+    errors.push("No column specified");
+  }
+  if (!req.body.error) {
+    errors.push("No error specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  const data = {
+    access_token: req.params.id,
+    column: req.body.column,
+    error: req.body.error,
+    file: req.body.file,
+    line: req.body.line,
+    message: req.body.message,
+  };
+  const query = "select * from users where access_token = ?";
+  db.get(query, data.access_token, (err, row) => {
+    if (err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+    const sql = "INSERT INTO events (user_id, message, file, line, column, error) VALUES (?,?,?,?,?,?)";
+    const params = [row.id, data.message, data.file, data.line, data.column, data.error];
+    db.run(sql, params, (error: { message: any; }, result: any) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        data,
+        message: "success",
+        row,
+      });
+    });
+  });
 });
 
 app.listen(process.env.PORT, () => {
